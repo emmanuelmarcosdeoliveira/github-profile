08e758391c3d077f37966ad36b8f2c9a63fe262e
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-03-13 23:48:16 -0300

    refactor: form removed corrected styles
    The Form that was not in use was removed and the color variables were corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
 import Perfil from "./components/Perfil";
-import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
 function App() {
-  const [formularioEstaVisivel, setFormularioEstaVisisvel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState("");
   return (
     <>
@@ -30,15 +28,6 @@ function App() {
           <ReposList nomeUsuario={nomeUsuario} />
         </>
       )}
-
-      {/* {formularioEstaVisivel && <Formulario />}
-
-      <button
-        onClick={() => setFormularioEstaVisisvel(!formularioEstaVisivel)}
-        type="button"
-      >
-        toggle form
-      </button> */}
     </>
   );
 }
